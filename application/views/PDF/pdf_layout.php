e1df99cf5879f1cc9248b46c762811e2989b1633
Herbert Lago <hebertpdl@gmail.com>
2019-06-29 12:03:53 -0300

    inicio implementação pdf

--- application/views/PDF/pdf_layout.php
+++ application/views/PDF/pdf_layout.php
@@ -1,10 +1,30 @@
-<h1> ESTE É O PDF </h1>
-<?php
-	echo $nome;
-	echo $cep;
-	echo $endereco;
-	echo $bairro;
-	echo $cidade;
-	echo $estado;
-	echo $cnpj
-?>
+<html><head>
+    <meta http-equiv="Content-Type" content="charset=utf-8" />
+    <link type="text/css" href="http://localhost/contratos-uniandrade/dist/styles/cooperation_term.css" rel="stylesheet" />
+</head><body>
+	<div>
+		<img src="https://www.uniandrade.br/wp-content/uploads/2019/05/logo-uni-rodape.png" alt="">
+		<p class="title">CENTRO UNIVERSITÁRIO CAMPOS DE ANDRADE - UNIANDRADE</p>
+		<p class="subtitle">Mantenedora: ASSOCIAÇÃO DE ENSINO VERSALHES<br/>Aprovado pelo Parecer CNE/MEC 083/99 em 28/01/99.<br/>Decreto Presidencial de 11/02/99, publicado no<br/>DOU de 12/02/99
+		</p>
+	</div>
+	<div>
+		<p class="title">TERMO DE COOPERAÇÃO TÉCNICA QUE ENTRE SI CELEBRAM O<br/>CENTRO UNIVERSITÁRIO CAMPOS DE ANDRADE<br/>E </p>
+		<p class="title"><?php echo $nome; ?></p>
+	</div>
+	<div style="text-align: justify;">
+		<p style="font-size: 15px; font-family: arial;">	O <strong>Centro Universitário Campos de Andrade</strong> – <strong>UNIANDRADE</strong>, estabelecido à Rua João Scuissiato, nº 01, Bairro
+			Santa Quitéria, Curitiba - Paraná, Fone: (41) 3219-4290, mantido pela Associação de Ensino Versalhes, pessoa
+			jurídica de direito privado inscrita no CNPJ: 79.732.194/0001-70 e-mail: estagio@uniandrade.edu.br, doravante
+			denominado <strong><em>INSTITUIÇÃO DE ENSINO</em></strong>, representada pelo <strong>Prof. José Campos de Andrade Filho</strong>, Cargo:
+			Reitor e Pró Reitora de Graduação deste Convênio: <strong>Profª. Me. Mari Elen Campos de Andrade</strong>; e de outro lado a </p>
+	</div>
+	<?php
+    echo $cep;
+    echo $endereco;
+    echo $bairro;
+    echo $cidade;
+    echo $estado;
+    echo $cnpj
+	?>
+</body></html>
--- application/views/formulario_view.php
+++ application/views/formulario_view.php
@@ -8,34 +8,34 @@
 						<div class="form-group">
 							<div class="form-group">
 								<label for="nome">Nome</label>
-								<input type="text" name="nome" class="form-control"  placeholder="Nome">
+								<input required type="text" name="nome" class="form-control"  placeholder="Nome">
 							</div>
 							<div class="form-group">
 								<label for="cep">CEP</label>
-								<input type="text" id="cep" name="cep" maxlength="9" class="form-control"  placeholder="00.000-000">
+								<input required type="text" id="cep" name="cep" maxlength="9" class="form-control"  placeholder="00.000-000">
 							</div>
 							<div class="form-group col-md-6 offset-md-4">
 								<a id="btn_consulta" class="btn btn-success">Consultar</a>
 							</div>
 							<div class="form-group">
 								<label for="endereco">Endereço</label>
-								<input type="text" id="endereco" name="endereco" class="form-control"  placeholder="Endereço">
+								<input required type="text" id="endereco" name="endereco" class="form-control"  placeholder="Endereço">
 							</div>
 							<div class="form-group">
 								<label for="bairro">Bairro</label>
-								<input type="text" id="bairro" name="bairro" class="form-control"  placeholder="Bairro">
+								<input required type="text" id="bairro" name="bairro" class="form-control"  placeholder="Bairro">
 							</div>
 							<div class="form-group">
 								<label for="estado">Estado</label>
-								<input type="text" id="estado" name="estado" maxlength="2" class="form-control"  placeholder="Sigla do estado">
+								<input required type="text" id="estado" name="estado" maxlength="2" class="form-control"  placeholder="Sigla do estado">
 							</div>
 							<div class="form-group">
 								<label for="cidade">Cidade</label>
-								<input type="text" id="cidade" name="cidade" class="form-control"  placeholder="Cidade">
+								<input required type="text" id="cidade" name="cidade" class="form-control"  placeholder="Cidade">
 							</div>
 							<div class="form-group">
 								<label for="cnpj">CNPJ/MF</label>
-								<input type="text" id="cnpj" name="cnpj" class="form-control"  placeholder="00.000.000/0000-00">
+								<input required type="text" id="cnpj" name="cnpj" class="form-control"  placeholder="00.000.000/0000-00">
 							</div>
 						</div>
 						<button type="submit" class="btn btn-primary ">Enviar</button>
